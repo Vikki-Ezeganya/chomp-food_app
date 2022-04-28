ef628287a3e823401a9438a90f8a7805d749bbdc
Vikki <vikkiezeganya@gmail.com>
2022-04-28 23:55:41 +0100

    refactored checkout controller

--- src/main/java/com/vikki/chompfooddelivery/controller/CheckOutController.java
+++ src/main/java/com/vikki/chompfooddelivery/controller/CheckOutController.java
@@ -1,4 +1,15 @@
 package com.vikki.chompfooddelivery.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/checkout")
+@AllArgsConstructor(onConstructor=@__(@Autowired))
 public class CheckOutController {
+
+
+
 }
